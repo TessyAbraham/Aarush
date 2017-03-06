2c8382dfaec7b07985ccb446b557dff39a8fa36a
TessyAbraham <tessy1116@gmail.com>
2017-03-06 14:20:56 +0530

    Create myadd.c

---
+++ myadd.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<math.h>
+void main(){
+float area;
+int l,b,h,radius,a;
+printf(" geometric shapes\n");
+print f("-------------------");
+printf("1-circle\n 2-rectangle\n 3-tringle\n 4-square\n ");
+printf("enter your choice 1-circle,2-rectangle,3- tringle,4-square,5-exist\n");
+switch(geometric)
+{
+case 1:printf("enter the radius\n");
+      scanf("%d",&radius);
+      area=radius*radius*3.14;
+      printf("area of the circle is %d",area);
+      break;
+case 2:printf("enter the breath\n");
+      printf("enter the hight"\n");
+      scanf("%d%d",&b,&h);
+      area=b*h;
+      printf("area of circle is %d",area);
+      break;
+case 3:printf("enter the sides of the tringle")
+       scanf("%d%d%",&b,&h);
+       area=1/2*b*h;
+       printf("area of the tringle is %d,area);
+       break;
+case 4:printf("enter the side of square");
+       scanf("%d", &a);
+       area=a*a
+
+default:"printf("invalid output"); 
+      
+}
+}
+
